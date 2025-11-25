c03c4d42eedcba92d5d53f4bb703be137d4d2297
hanguyen4204 <22071008@vnu.edu.vn>
2025-11-25 14:20:57 +0700

    Update connection1.php

--- connection1.php
+++ connection1.php
@@ -1,5 +1,5 @@
 <?php
-$dsn = "mysql:host=localhost;dbname=loginreg;charset=utf8mb4";
+$dsn = "mysql:host=localhost;dbname=zoo_manage;charset=utf8mb4";
 $db_user = "root";
 $db_pass = "";
 $options = [ PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC ];
